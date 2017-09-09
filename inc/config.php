660b4ba1442ec17c15e398b9bd4dd5b290b0a786
Zeradam-Fantaye <zeradam.fantaye@gmail.com>
2017-09-09 16:48:47 -0700

    Everything is added

---
+++ inc/config.php
@@ -0,0 +1,15 @@
+<?php 
+
+	// If there is no constant defined called __CONFIG__, do not load this file 
+	if(!defined('__CONFIG__')) {
+		exit('You do not have a config file');
+		
+		
+		// Sometimes we might do like .... show the user a 404 page instead of exiting.
+	}
+
+
+	// Our config is below
+
+
+?>
